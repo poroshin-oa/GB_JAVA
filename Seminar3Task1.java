4774a33f95bed186ae58441a6258d04f4d4b2294
oleg <poroshin.oa72@gmail.com>
2023-02-02 21:40:56 +0300

    Добавлен вывод

--- Seminar3Task1.java
+++ Seminar3Task1.java
@@ -15,25 +15,32 @@ public class Homework {
     }
 
     // Нужно удалить из него четные числа
-    public static List<Integer> removeEvenValue(List<Integer> list){
+    public static List<Integer> removeEvenValue(List<Integer> rem_list){
+        ArrayList<Integer> result_rem_even_list = new ArrayList<Integer>();
 
-        return new ArrayList<>();
+        for (int i = 0; i < rem_list.size(); i++) {
+            if(rem_list.get(i) % 2 == 0){
+
+            }
+        }
+
+        return rem_list;
     }
 
     // Найти минимальное значение
-    public Integer getMin(List<Integer> list){
+    public static Integer getMin(List<Integer> list){
 
         return 0;
     }
 
     // Найти максимальное значение
-    public Integer getMax(List<Integer> list){
+    public static Integer getMax(List<Integer> list){
 
         return 0;
     }
 
     // Найти среднее значение
-    public Integer getAverage(List<Integer> list){
+    public static Integer getAverage(List<Integer> list){
 
         return 0;
     }
@@ -47,8 +54,19 @@ public class Homework {
         System.out.print(new_int_list);
         System.out.println("\n");
 
-        System.out.println("Удаленные четные числа: ");
-        System.out.print(rem_even_list);
-        System.out.println("\n");
+        System.out.print("Минимальное значение: ");
+        System.out.println(getMin(new_int_list));
+
+        System.out.print("Максимальное значение: ");
+        System.out.println(getMax(new_int_list));
+
+        System.out.print("Среднее значение: ");
+        System.out.println(getAverage(new_int_list));
+        System.out.print("\n");
+
+
+//        System.out.println("Удаленные четные числа: ");
+//        System.out.print(rem_even_list);
+//        System.out.println("\n");
     }
 }
